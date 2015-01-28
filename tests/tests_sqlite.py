bec247aad278e731fa95e0f68c744553d7b8ca6d
michaeljohnbarr <micbarr@gmail.com>
2015-01-28 00:56:21 -0500

    Create tests_sqlite.py
    Test settings for sqlite

---
+++ tests/tests_sqlite.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+INSTALLED_APPS = (
+    'tests.myapp',
+)
+
+SITE_ID=1,
+
+SECRET_KEY='secret'
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+)
